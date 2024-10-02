84c7b788e97e04d740f542b3556d30037d4e1523
Cesar Patino <cpatino@copaair.com>
2024-10-01 23:24:22 -0500

    only image, icon and hero b

--- src/lib/components/site/text-content/hero-a/hero-a.svelte
+++ src/lib/components/site/text-content/hero-a/hero-a.svelte
@@ -2,34 +2,48 @@
 	import { getDirectusImage } from '$lib/components/directus/stopover/utils';
 	import { Button } from '$lib/components/ui/foundations/button';
 	import { getTypographyVariant } from '$lib/components/ui/foundations/typography';
+	import { Breadcrum } from '$lib/components/site/navigation/breadcrum';
+	import { page } from '$app/stores';
 	import type { TextContentSchema } from '$lib/directus/text-content';
+	import { SVG } from '$lib/components/ui/foundations/icon';
 
 	export let item: TextContentSchema;
 
 	const {
+		image,
 		translations: {
-			0: { title, description, media, call_to_actions }
+			0: { title, description, media, icon, call_to_actions }
 		}
 	} = item;
 
 	const ctas = call_to_actions || [];
+
+	const pageSettings = $page.data.page;
+
+	const mainImage = media || image;
 </script>
 
-<div class="container-grid grid-rows-[2fr_64px_auto]">
-	<div class="col-span-full col-start-1 row-span-2 row-start-1">
-		<picture>
-			{#if media}
-				<img src="{getDirectusImage(media)}&key=square-600" alt="" class="h-auto w-full" />
-			{/if}
-		</picture>
+<div class="container-grid auto-rows-auto">
+	<div class="col-span-ful row-start-1 h-28"></div>
+	<div class="col-span-full col-start-1 row-span-3 row-start-1">
+		{#if mainImage}
+			<picture>
+				<img src="{getDirectusImage(mainImage)}&key=square-600" alt="" class="h-auto w-full" />
+			</picture>
+		{/if}
 	</div>
+	{#if icon}
+		<SVG data={icon.code} class="col-start-2 row-start-2 size-28 fill-black"></SVG>
+	{/if}
 	<div
-		class="col-span-1 col-start-2 row-span-2 row-start-2 space-y-5 rounded-2xl bg-primary p-4 shadow-lg"
+		class="col-span-1 col-start-2 row-span-2 row-start-3 my-8 space-y-5 rounded-2xl bg-primary p-4 shadow-lg"
 	>
 		<h1 class={getTypographyVariant('display-big', 'text-grey-50')}>{title}</h1>
-		<div class="text-grey-50">
-			{@html description}
-		</div>
+		{#if description}
+			<div class="text-grey-50">
+				{@html description}
+			</div>
+		{/if}
 		{#if call_to_actions}
 			<ul class="flex gap-2">
 				{#each ctas as cta}
@@ -41,3 +55,8 @@
 		{/if}
 	</div>
 </div>
+{#if pageSettings}
+	<div class="container mx-auto mt-8">
+		<Breadcrum item={pageSettings}></Breadcrum>
+	</div>
+{/if}
---
+++ src/lib/components/site/text-content/hero-b/hero-b.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import type { TextContentSchema } from '$lib/directus/text-content';
+	import { page } from '$app/stores';
+	import { getTypography } from '$lib/components/ui/foundations/typography';
+	import { Breadcrum } from '../../navigation/breadcrum';
+	import { getTypographyVariant } from '$lib/components/ui/foundations/typography';
+	import { getDirectusImage } from '$lib/components/directus/stopover/utils';
+	import { SVG } from '$lib/components/ui/foundations/icon';
+	import { Button } from '$lib/components/ui/foundations/button';
+
+	export let item: TextContentSchema;
+
+	const {
+		image,
+		translations: {
+			0: { title, description, media, icon, call_to_actions }
+		}
+	} = item;
+
+	const pageSettings = $page.data.page;
+
+	const mainImage = media || image;
+</script>
+
+<div class="container-grid auto-rows-auto">
+	<div class="row-start-1 h-28"></div>
+	{#if pageSettings}
+		<div class="col-start-2 row-start-2">
+			<Breadcrum item={pageSettings} variant="invert"></Breadcrum>
+		</div>
+	{/if}
+	<h1 class={getTypographyVariant('display-big', 'col-start-2 row-start-3 mb-6 text-grey-50')}>
+		{title}
+	</h1>
+	<div class="col-span-full col-start-1 row-span-3 row-start-5 bg-backgound-paper"></div>
+	s
+	{#if mainImage}
+		<div class="col-start-2 row-span-2 row-start-4">
+			<picture>
+				<img
+					src="{getDirectusImage(mainImage)}&key=3-4x600"
+					alt=""
+					class="h-auto w-full rounded-2xl shadow-lg"
+				/>
+			</picture>
+		</div>
+	{/if}
+	{#if icon}
+		<SVG data={icon.code} class="col-start-2 row-span-2 row-start-4 size-28 fill-black py-2"></SVG>
+	{/if}
+	{#if description}
+		<div class={getTypography('body', 'body', 'col-start-2 row-start-6 mb-spacious mt-6')}>
+			{@html description}
+		</div>
+	{/if}
+	{#if call_to_actions}
+		<ul class="col-start-2 row-start-7 mt-6 flex gap-2">
+			<li class="flex-grow">
+				{#each call_to_actions as cta, i}
+					<Button
+						href={cta.link}
+						target={cta.open_in}
+						variant={i > 0 ? 'outline-primary-main' : 'solid-primary-main'}
+					>
+						{cta.text}
+					</Button>
+				{/each}
+			</li>
+		</ul>
+	{/if}
+</div>
