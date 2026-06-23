b305cd621c6a68069bf5f9f591e455505fd2b5ef
César Patiño <cpatino@copaair.com>
2026-06-23 14:38:18 -0500

    fix: harden prerender data handling

--- src/lib/directus/content-group.ts
+++ src/lib/directus/content-group.ts
@@ -1,27 +1,27 @@
-import { z } from "zod";
-import { navigationQuery, navigationSchema } from "./navigation";
-import { textContentQuery, textContentSchema } from "./text-content";
-import { stopoverHotelModuleQueryFields, stopoverHotelModuleSchema } from "./stopover_hotel_module";
+import { z } from 'zod';
+import { navigationQuery, navigationSchema } from './navigation';
+import { textContentQuery, textContentSchema } from './text-content';
+import { stopoverHotelModuleQueryFields, stopoverHotelModuleSchema } from './stopover_hotel_module';
 import {
 	stopoverMixedExperienceModuleQueryFields,
 	stopoverMixedExperienceModuleSchema
 } from './stopover_mixed_experience_module';
-import { formQueryFields, formSchema } from "./forms";
+import { formQueryFields, formSchema } from './forms';
 
 const contentGroupTranslation = z.object({
-  languages_code: z.string().optional(),
-  title: z.string(),
-})
+	languages_code: z.string().optional(),
+	title: z.string()
+});
 
 const contentGroupCollectionName = z.union([
-  z.literal('navigation'),
-  z.literal('Text_Content'),
-  z.literal('form'),
-  z.literal('content_group'),
-  z.literal('stopover_hotel_module'),
-  z.literal('stopover_mixed_experience_module'),
-  z.literal('stopover_mixed_experiece_module')
-])
+	z.literal('navigation'),
+	z.literal('Text_Content'),
+	z.literal('form'),
+	z.literal('content_group'),
+	z.literal('stopover_hotel_module'),
+	z.literal('stopover_mixed_experience_module'),
+	z.literal('stopover_mixed_experiece_module')
+]);
 
 const contentGroupItems = z.union([
 	navigationSchema,
@@ -29,56 +29,49 @@ const contentGroupItems = z.union([
 	stopoverHotelModuleSchema,
 	stopoverMixedExperienceModuleSchema,
 	formSchema
-])
+]);
 
 const contentGroupContentSchema = z.object({
-  item: contentGroupItems,
-  component: z.string().nullable(),
-  collection: contentGroupCollectionName
-})
+	item: contentGroupItems,
+	component: z.string().nullable(),
+	collection: contentGroupCollectionName
+});
 
 const contentGroupSchema = z.object({
-  translations: contentGroupTranslation.array().nullable(),
-  content: contentGroupContentSchema.array().nullable(),
-})
+	translations: contentGroupTranslation.array().nullable(),
+	content: contentGroupContentSchema.array().nullable()
+});
 
 const contentGroupQueryFields = [
-  {
-    'translations': [ 'title' ],
-  },
-  {
-    'content': [
-      'collection',
-      'component',
-      {
-        'item': {
-          'navigation': navigationQuery,
-          'Text_Content': textContentQuery,
-          'form': formQueryFields,
-          'stopover_hotel_module': stopoverHotelModuleQueryFields,
-          'stopover_mixed_experience_module': stopoverMixedExperienceModuleQueryFields
-        }
-      }
-    ]
-  }
-]
+	{
+		translations: ['title']
+	},
+	{
+		content: [
+			'collection',
+			'component',
+			{
+				item: {
+					navigation: navigationQuery,
+					Text_Content: textContentQuery,
+					form: formQueryFields,
+					stopover_hotel_module: stopoverHotelModuleQueryFields,
+					stopover_mixed_experience_module: stopoverMixedExperienceModuleQueryFields
+				}
+			}
+		]
+	}
+];
 
-type ContentGroupSchema = z.infer<typeof contentGroupSchema>
+type ContentGroupSchema = z.infer<typeof contentGroupSchema>;
 
-type ContentGroupContent = z.infer<typeof contentGroupContentSchema>
+type ContentGroupContent = z.infer<typeof contentGroupContentSchema>;
 
-type ContentGroupItems = z.infer<typeof contentGroupItems>
+type ContentGroupItems = z.infer<typeof contentGroupItems>;
 
-const isContentGroupSchema = (value: unknown): value is ContentGroupSchema => contentGroupSchema.safeParse(value).success
+const isContentGroupSchema = (value: unknown): value is ContentGroupSchema =>
+	contentGroupSchema.safeParse(value).success;
 
-export {
-  contentGroupSchema,
-  isContentGroupSchema,
-  contentGroupQueryFields
-}
+export { contentGroupSchema, isContentGroupSchema, contentGroupQueryFields };
 
-export type {
-  ContentGroupSchema,
-  ContentGroupContent,
-  ContentGroupItems
-}
+export type { ContentGroupSchema, ContentGroupContent, ContentGroupItems };
